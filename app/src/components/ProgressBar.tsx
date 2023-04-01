41ce7f50f139aeb3a13f73245d282c1d495f30b1
sytzez <sytzez@gmail.com>
2023-04-02 00:41:30 +0100

    Show progress bars

---
+++ app/src/components/ProgressBar.tsx
@@ -0,0 +1,10 @@
+export default function ProgressBar({ progress }: { progress: number }) {
+  return (
+    <div className="bg-gradient-to-r from-red-900 to-red-800 rounded-full h-8 shadow-md">
+      <div
+        className="bg-gradient-to-r from-yellow-300 to-yellow-200 rounded-full h-8"
+        style={{width: `${progress * 100}%`, minWidth: '2rem'}}
+      />
+    </div>
+  )
+}
--- app/src/pages/GroupDetail.tsx
+++ app/src/pages/GroupDetail.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react'
 import { Group } from '../schemata/group'
 import { useNavigate, useParams } from 'react-router-dom'
 import { ApiContext } from '../contexts/api-context'
+import ProgressBar from "../components/ProgressBar";
 
 export default function GroupDetail() {
   const api = useContext(ApiContext)
@@ -23,6 +24,10 @@ export default function GroupDetail() {
 
   if (!group) return <p className="text-white">Loading</p>
 
+  const canAssignSecretSints = group.users
+    && group.users.length >= 3
+    && group.wishlist_count === group.users.length
+
   return (
     <div className="flex gap-2 flex-col">
       <h1 className="text-white text-2xl font-bold mb-4">{group.title}</h1>
@@ -32,7 +37,7 @@ export default function GroupDetail() {
       >
         Edit your wishlist
       </button>
-      <h2 className="text-white text-lg font-bold">Participants</h2>
+      <h2 className="text-white text-lg font-bold mt-2">Participants</h2>
       <ul>
         {group.users.map((user) => (
           <li
@@ -54,16 +59,27 @@ export default function GroupDetail() {
           >
             Add participants
           </button>
-          <h2 className="text-white text-lg font-bold">Progress</h2>
+          <h2 className="text-white text-lg font-bold mt-2">Progress</h2>
+          {group.users!.length < 3
+            &&  <div className="mb-2">
+              <p className="text-white mb-2">You need a least 3 participants to play Secret Sint.</p>
+              <ProgressBar progress={group.users!.length / 3} />
+            </div>}
+          {group.users!.length > 1
+            && <div className="mb-2">
+              <p className="text-white">{group.wishlist_count!} out of {group.users!.length} people have written their wishlist.</p>
+              <ProgressBar progress={group.wishlist_count! / group.users!.length} />
+            </div>}
           <button
             type="button"
-            className="rounded-full font-bold p-4 bg-yellow-400 hover:bg-yellow-500 shadow-lg"
+            className="rounded-full font-bold p-4 bg-yellow-400 hover:bg-yellow-500 shadow-lg disabled:bg-gray-400"
+            disabled={!canAssignSecretSints}
           >
             Assign Secret Sints!
           </button>
-          <p className="text-white">
+          <p className="text-white mb-2">
             Once the Secret Sints have been assigned, you can not invite any
-            more participants, nor can anyone edit their wishlists.
+            more participants, nor can anyone change their wishlists.
           </p>
           <button
             className="rounded-full border border-white text-white p-4 bg-red-600 hover:bg-red-700 shadow-lg mt-4"
