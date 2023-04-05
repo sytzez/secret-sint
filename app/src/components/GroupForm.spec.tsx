ce0f98394f55a286d24c17b124b125080c7daecf
sytzez <sytzez@gmail.com>
2023-04-05 17:27:03 +0100

    Test GroupForm

---
+++ app/src/components/GroupForm.spec.tsx
@@ -0,0 +1,41 @@
+import { describe, expect, it, vi } from 'vitest'
+import { fireEvent, render } from '@testing-library/react'
+import GroupForm from './GroupForm'
+
+describe('<GroupForm />', () => {
+  it('can submit a groupRequest', async () => {
+    const submitMock = vi.fn()
+    const { unmount, findByText, findByLabelText } = render(
+      <GroupForm
+        onSubmit={submitMock}
+        isLoading={false}
+        submitLabel="Submit group"
+      />,
+    )
+
+    const nameElement = await findByLabelText('Name')
+    fireEvent.change(nameElement, { target: { value: 'A new group' } })
+
+    const submitElement = await findByText('Submit group')
+    fireEvent.click(submitElement)
+
+    expect(submitMock).toBeCalledWith({ title: 'A new group' })
+    unmount()
+  })
+  it('does not submit when the form is not filled in', async () => {
+    const submitMock = vi.fn()
+    const { unmount, findByText } = render(
+      <GroupForm
+        onSubmit={submitMock}
+        isLoading={false}
+        submitLabel="Submit group"
+      />,
+    )
+
+    const submitElement = await findByText('Submit group')
+    fireEvent.click(submitElement)
+
+    expect(submitMock).not.toBeCalled()
+    unmount()
+  })
+})
