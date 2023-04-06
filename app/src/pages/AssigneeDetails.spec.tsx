4a1be5beb5f6f29f65261b0db22106f6b7dd7f17
sytzez <sytzez@gmail.com>
2023-04-06 15:36:31 +0100

    Test for edit present status route

--- app/src/pages/AssigneeDetails.spec.tsx
+++ app/src/pages/AssigneeDetails.spec.tsx
@@ -5,14 +5,14 @@ import { Api } from '../api/create-api'
 import { MemoryRouter } from 'react-router-dom'
 import App from '../App'
 
-describe('/group/:groupId/assignee', () => {
+describe('/groups/:groupId/assignee', () => {
   it("fetches and shows the assignee's details", async () => {
-    const mockAssigneeCall = vi.fn(() => ({
-      user: { email: 'bob@test.com' },
-      wishlist: 'Many things',
-    }))
-
-    const mockApi = { assignee: mockAssigneeCall }
+    const mockApi = {
+      assignee: vi.fn(() => ({
+        user: { email: 'bob@test.com' },
+        wishlist: 'Many things',
+      })),
+    }
 
     const { unmount, queryByText } = render(
       <ApiContext.Provider value={mockApi as unknown as Api}>
@@ -23,7 +23,7 @@ describe('/group/:groupId/assignee', () => {
     )
     await new Promise((r) => setTimeout(r))
 
-    expect(mockAssigneeCall).toBeCalledWith(123)
+    expect(mockApi.assignee).toBeCalledWith(123)
 
     expect(queryByText('bob@test.com')).not.toBeNull()
     expect(queryByText('Many things')).not.toBeNull()
---
+++ app/src/pages/EditPresentStatus.spec.tsx
@@ -0,0 +1,44 @@
+import { describe, expect, it, vi } from 'vitest'
+import { fireEvent, render } from '@testing-library/react'
+import { ApiContext } from '../contexts/api-context'
+import { MemoryRouter } from 'react-router-dom'
+import App from '../App'
+import { Api } from '../api/create-api'
+
+describe('/groups/:groupId/present-status', async () => {
+  it('can show and update the present status', async () => {
+    const mockApi = {
+      participation: vi.fn(() => ({
+        present_status: 'not_started' as const,
+        user: { email: 'alice@test.com' },
+      })),
+      updateParticipation: vi.fn(),
+    }
+
+    const { unmount, queryByText, findByLabelText } = render(
+      <ApiContext.Provider value={mockApi as unknown as Api}>
+        <MemoryRouter initialEntries={['/groups/123/present-status']}>
+          <App />
+        </MemoryRouter>
+      </ApiContext.Provider>,
+    )
+    await new Promise((r) => setTimeout(r))
+
+    expect(mockApi.participation).toBeCalledWith(123)
+
+    expect(queryByText('Not started')).not.toBeNull()
+
+    const selectElement = await findByLabelText('The status of your present')
+    fireEvent.change(selectElement, { target: { value: 'ordered' } })
+
+    const submitButton = queryByText('Update present status')!
+    fireEvent.click(submitButton)
+    await new Promise((r) => setTimeout(r))
+
+    expect(mockApi.updateParticipation).toBeCalledWith(123, {
+      present_status: 'ordered',
+    })
+
+    unmount()
+  })
+})
