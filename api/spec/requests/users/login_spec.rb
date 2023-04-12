c8dba3a7da3e1a4b5a2d8b8e9f17c2b7afaf5e7b
sytzez <sytzez@gmail.com>
2023-04-12 17:53:44 +0100

    Test login route

---
+++ api/spec/requests/users/login_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'POST /login', type: :request do
+  let(:user) { create :user }
+
+  context 'with valid params' do
+    let(:params) { { user: { email: user.email, password: user.password } } }
+
+    it 'logs in' do
+      post user_session_path, params: params, as: :json
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'with invalid params' do
+    let(:params) { { user: { email: user.email, password: 'wrong password' } } }
+
+    it 'returns an error' do
+      post user_session_path, params: params, as: :json
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
