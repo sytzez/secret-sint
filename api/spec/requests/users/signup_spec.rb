e8b7d039540bdfa380f18a7b018d55852a978961
sytzez <sytzez@gmail.com>
2023-04-12 17:51:16 +0100

    Test signup request

---
+++ api/spec/requests/users/signup_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'POST /signup', type: :request do
+  context 'with valid params' do
+    let(:params) { { user: { email: Faker::Internet.email, password: 'supersecret' } } }
+
+    it 'creates a user' do
+      expect do
+        post user_registration_path, params: params, as: :json
+      end.to change(User, :count).by(1)
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'with invalid params' do
+    let(:params) { { user: { email: 'notanemail', password: 'short' } } }
+
+    it 'returns an error' do
+      expect do
+        post user_registration_path, params: params, as: :json
+      end.not_to change(User, :count)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+end
