b6cd8915458df5d970898b1338986f1fe8704957
sytzez <sytzez@gmail.com>
2023-04-12 20:49:50 +0100

    Test that a group page looks different if it was started

--- app/src/pages/GroupDetail.spec.tsx
+++ app/src/pages/GroupDetail.spec.tsx
@@ -6,19 +6,21 @@ import { MemoryRouter } from 'react-router-dom'
 import App from '../App'
 
 describe('/groups/:groupId', () => {
+  const group = {
+    id: 123,
+    title: 'My group',
+    has_started: false,
+    users: [
+      { email: 'alice@test.com' },
+      { email: 'bob@test.com' },
+      { email: 'charlie@test.com' },
+    ],
+    wishlist_count: 3,
+  }
+
   it('can show a group and assign secret sints', async () => {
     const mockApi = {
-      group: vi.fn(() => ({
-        id: 123,
-        title: 'My group',
-        has_started: false,
-        users: [
-          { email: 'alice@test.com' },
-          { email: 'bob@test.com' },
-          { email: 'charlie@test.com' },
-        ],
-        wishlist_count: 3,
-      })),
+      group: vi.fn(() => group),
       assignSecretSints: vi.fn(),
     }
 
@@ -33,6 +35,8 @@ describe('/groups/:groupId', () => {
 
     expect(mockApi.group).toBeCalledWith(123)
 
+    expect(queryByText('View your assignee')).toBeNull()
+    expect(queryByText('Update your present status')).toBeNull()
     expect(queryByText('My group')).not.toBeNull()
     expect(queryByText('alice@test.com')).not.toBeNull()
     expect(queryByText('bob@test.com')).not.toBeNull()
@@ -42,6 +46,29 @@ describe('/groups/:groupId', () => {
     fireEvent.click(assignSintsButton)
 
     expect(mockApi.assignSecretSints).toBeCalledWith(123)
+
+    unmount()
+  })
+
+  it('shows buttons to view your assignee and update your present status if the group has started', async () => {
+    const mockApi = {
+      group: vi.fn(() => ({ ...group, has_started: true })),
+    }
+
+    const { unmount, queryByText } = render(
+      <ApiContext.Provider value={mockApi as unknown as Api}>
+        <MemoryRouter initialEntries={['/groups/123']}>
+          <App />
+        </MemoryRouter>
+      </ApiContext.Provider>,
+    )
+    await new Promise((r) => setTimeout(r))
+
+    expect(mockApi.group).toBeCalledWith(123)
+
+    expect(queryByText('View your assignee')).not.toBeNull()
+    expect(queryByText('Update your present status')).not.toBeNull()
+    expect(queryByText('Assign Secret Sints!')).toBeNull()
 
     unmount()
   })
