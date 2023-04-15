2f0dc95539e301e2a507d49f9ca1c2abc2d75176
sytzez <sytzez@gmail.com>
2023-04-15 16:20:27 +0100

    Add production URL to CORS origins list

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'http://127.0.0.1:5173'
+    origins 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://secret-sint.vercel.app'
 
     resource '*',
              headers: :any,
